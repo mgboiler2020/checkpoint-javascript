86aa82b98b997e225f838edd9f2ca635d74b9569
MattG2000 <GuffyMatthewO@JohnDeere.com>
2021-01-08 10:04:32 -0500

    solved 5.

--- fundamentals.js
+++ fundamentals.js
@@ -25,7 +25,11 @@ for (i =0; i<foods.length; i++) {
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
+var instructor = {
+    lastname:"smith", 
+    height:7, 
+    weight:180
+},
 
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
