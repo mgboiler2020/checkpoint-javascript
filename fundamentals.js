6c0e7f5de2d9bb8dced326caff8f0305b2c1a1a9
MattG2000 <GuffyMatthewO@JohnDeere.com>
2021-01-08 09:59:31 -0500

    solved 4.

--- fundamentals.js
+++ fundamentals.js
@@ -17,7 +17,10 @@ var favoriteFoods = [];
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-
+var i = 0;
+for (i =0; i<foods.length; i++) {
+    favoriteFoods[i]=foods[i];
+}
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
